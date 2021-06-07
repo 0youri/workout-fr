eed22222dfae9add5aabed7e646cf86b636ecdc0
youri <yourinovikov@gmail.com>
2021-06-07 17:21:06 +0200

    update

--- index.php
+++ index.php
@@ -6,35 +6,39 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" 
         integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.js"></script>
-        <script src="script.js"></script>
+        <script type="text/javascript" src="js/script.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     </head>
         
     <body>
         <?php 
-            include('view/menu.php');
+            include('view/viewMenu.php');
 
             if(isset($_GET['page'])) // Choix de la page
             {
                 $page = $_GET['page'];
                 
-                if(isset($routes[$page])) {
+                if(isset($routes[$page]))
+                {
                     $controller = $routes[$page]['controller'];
                     $view = $routes[$page]['view'];
                     include('controller/' . $controller . '.php');
                     include('view/' . $view . '.php');
                 }
-                else {
-                    include('static/accueil.php');
+                else
+                {
+                    include('view/viewAccueil.php');
+                    include('controller/controllerAccueil.php');
                 }
             }
-            else {
-                include('static/accueil.php');
+            else
+            {
+                include('view/viewAccueil.php');
+                include('controller/controllerAccueil.php');
             }
-
-            include('static/footer.php');
         ?>
     </body>
 </html>
+
 
 
---
+++ view/viewMenu.php
@@ -0,0 +1,25 @@
+<div class="container-fluid bg-dark">
+            <header>
+                <h1 class="fw-bold text-white" style="text-align: center">Workout</h1>
+            </header>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-top">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+                    aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Ajouter un workout</a>
+                            </li>
+                        </ul>
+                        <div class="d-flex">
+                            <input type="text" class="form-control me-2" id="search" placeholder="Ex: Fullbody" onChange="Recherche();" >
+                            <button class="btn btn-outline-light bi bi-search" type="button" onclick="Recherche();"></button>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+</div> 
