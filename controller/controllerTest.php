78d62dfa9e433fafd3adc5895ff30cd9f8b7f2e9
Youri <yourinovikov@gmail.com>
2021-07-01 10:01:11 +0200

    update

--- controller/controllerTest.php
+++ controller/controllerTest.php
@@ -1,8 +1,10 @@
 <br>
 <div class="container">
 <?php
-$conn_string = "host=ec2-54-228-9-90.eu-west-1.compute.amazonaws.com port=5432 dbname=df3npd7h1f5dkc user=eiqaobprkliovt password=6b4f68850425308602fcd0497e45515a6ff2c9abbbd0cc51d581ac1dfde4817d";
-echo pg_connect($conn_string);
-echo "<br>ici";
+$connStr = "host=ec2-54-228-9-90.eu-west-1.compute.amazonaws.com port=5432 dbname=df3npd7h1f5dkc user=eiqaobprkliovt password=6b4f68850425308602fcd0497e45515a6ff2c9abbbd0cc51d581ac1dfde4817d";
+$conn = pg_connect($connStr);
+$requete = "CREATE TABLE test_tab (vector  int[][]);";
+$result = pg_query($conn, $requete);
+var_dump(pg_fetch_all($result));
 ?>
 </div>
