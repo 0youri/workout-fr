7b966a1b115e39b6951208fff3e3edcd908d2bb8
Youri <yourinovikov@gmail.com>
2021-07-01 09:57:54 +0200

    update

--- controller/controllerTest.php
+++ controller/controllerTest.php
@@ -1,5 +1,8 @@
 <br>
+<div class="container">
 <?php
-$conn_string = "host=ec2-54-228-9-90.eu-west-1.compute.amazonaws. port=5432 dbname=df3npd7h1f5dkc user=eiqaobprkliovt password=6b4f68850425308602fcd0497e45515a6ff2c9abbbd0cc51d581ac1dfde4817d";
-pg_connect($conn_string);
+$conn_string = "host=ec2-54-228-9-90.eu-west-1.compute.amazonaws.com port=5432 dbname=df3npd7h1f5dkc user=eiqaobprkliovt password=6b4f68850425308602fcd0497e45515a6ff2c9abbbd0cc51d581ac1dfde4817d";
+echo pg_connect($conn_string);
+echo "<br>ici";
 ?>
+</div>
