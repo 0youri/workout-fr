3b4acb29c1766b007f4ff125bc81ee742ef4d1fb
Youri <yourinovikov@gmail.com>
2021-07-02 12:09:20 +0200

    d

--- model/onload.php
+++ model/onload.php
@@ -1,11 +1,12 @@
 <?php
     $connect = connectDB();
-
+    /*
     $request = "SELECT * FROM public.allworkout";
-    
+
     $allData = pg_query($connect,$request);
     if ( !$allData ) printf("Error model/onload.php > pg_query(...)");
 
     $data = pg_fetch_assoc($allData);
     if ( !$data ) printf("Error model/onload.php > pg_fetch_assoc(...)");
+    */
 ?>
