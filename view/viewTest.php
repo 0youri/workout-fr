2be27b380d4ae27a98cd50019d0d52fbb14efd6b
Youri <yourinovikov@gmail.com>
2021-07-04 16:58:10 +0200

    test

--- view/viewTest.php
+++ view/viewTest.php
@@ -2,7 +2,7 @@
     echo '<br> $request';
     $request = "SELECT id FROM public.w1 WHERE muscle = 'Jambes';"
     echo '<br> if';
-    if ( !pg_connection_busy($connect) ) pg_send_query($connect,$request);
+    //if ( !pg_connection_busy($connect) ) pg_send_query($connect,$request);
     echo '<br> $tableSQL';
     /*
     $tableSQL = pg_get_result($connect);
