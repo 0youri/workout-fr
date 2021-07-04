cd0ab6346a813d29144bbbf791958ffa59859c06
Youri <yourinovikov@gmail.com>
2021-07-04 13:53:19 +0200

    upd

--- model/includes.php
+++ model/includes.php
@@ -3,8 +3,11 @@
 // connect to the DataBase
 function connectDB()
 {
+	echo "here 1";
 	$connectStr = "host=".$HOST." port=".$PORT." dbname=".$DBNAME." user=".$USER." password=".$PASSWORD."";
+	echo "here 2";
 	$connect = pg_connect($connectStr);
+	echo "here 3";
 	return $connect;
 }
 
