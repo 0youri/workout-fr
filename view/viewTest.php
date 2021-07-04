1e956733d044ce5aa78f1fcb4802ed2d06cbd442
Youri <yourinovikov@gmail.com>
2021-07-04 16:43:35 +0200

    test

--- view/viewTest.php
+++ view/viewTest.php
@@ -1,11 +1,14 @@
 <?php
-
+    echo '<br> $connect';
     $connect = connectDB();
+    echo '<br> $request';
     $request = "SELECT id FROM public.w1 WHERE muscle = 'Jambes';"
+    echo '<br> if';
     if ( !pg_connection_busy($connect) ) pg_send_query($connect,$request);
+    echo '<br> $tableSQL';
     $tableSQL = pg_get_result($connect);
-    echo '$tableSQL : '.$tableSQL;
     $row = pg_fetch_assoc($tableSQL);
     echo '$row["id"] : '.$row['id'];
+    echo "here after all";
 
 ?>
