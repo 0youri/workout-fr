1a4946d2d6164626407ca78a49058e6d48cb51dc
Youri <yourinovikov@gmail.com>
2021-07-04 14:26:43 +0200

    upd

--- controller/controllerWorkout.php
+++ controller/controllerWorkout.php
@@ -1,52 +1,45 @@
 <?php
     $workoutHTML = "";
-    echo '<br> $connect in out  : '.$connect;
     $allworkoutSQL = recoverTableDataDB("allworkout",$connect);
-    echo '<br> $allworkoutSQL : '.$allworkoutSQL;
-    while ( $data = pg_fetch_assoc($allworkoutSQL) )
+    while ( $dataAllWorkout = pg_fetch_assoc($allworkoutSQL) )
     {
-        echo "here while";
-        echo "<br>id : ".$data['id'];
-        echo " - name : ".$data['name'];
-        /*
-        echo '<div id="w'.$row['id'].'">
+        
+        $workoutHTML = $workoutHTML.'<div id="w'.$dataAllWorkout['id'].'">
             <br>
             <div class="card-body border rounded ">
                 <div class="position-relative">
                     <div class="text-black fs-5">
-                        <strong>#'.$explore[0].' - '.$explore[1].'</strong>
+                        <strong>#'.$dataAllWorkout['id'].' - '.$dataAllWorkout['name'].'</strong>
                         <span class="position-absolute end-0">
-                            <button class="btn btn-dark bi bi-info-circle-fill" type="button" id="infow'.$explore[0].'" 
-                            onclick="AfficherCollapse(`collapsew'.$explore[0].'`);"></button>
-                            <a class="btn btn-dark bi bi-bar-chart-fill" id="statsw'.$explore[0].'" 
-                            href="index.php?page=stats&w='.$explore[0].'"></a>
-                            <button class="btn btn-dark bi bi-gear-fill" type="button" id="modifw'.$explore[0].'" ></button>
-                            <a class="btn btn-dark bi bi-plus-circle-fill" id="addw'.$explore[0].'" 
-                            href="index.php?page=add&w='.$explore[0].'&etat=-1"></a>
+                            <button class="btn btn-dark bi bi-info-circle-fill" type="button" id="infow'.$dataAllWorkout['id'].'" 
+                            onclick="AfficherCollapse(`collapsew'.$dataAllWorkout['id'].'`);"></button>
+                            <a class="btn btn-dark bi bi-bar-chart-fill" id="statsw'.$dataAllWorkout['id'].'" 
+                            href="index.php?page=stats&w='.$dataAllWorkout['id'].'"></a>
+                            <button class="btn btn-dark bi bi-gear-fill" type="button" id="modifw'.$dataAllWorkout['id'].'" ></button>
+                            <a class="btn btn-dark bi bi-plus-circle-fill" id="addw'.$dataAllWorkout['id'].'" 
+                            href="index.php?page=add&w='.$dataAllWorkout['id'].'&etat=-1"></a>
 
                         </span>
                     </div>
                 </div>
             </div>
-            <div id="collapsew'.$explore[0].'" style="display:none;">
+            <div id="collapsew'.$dataAllWorkout['id'].'" style="display:none;">
                     <br>
                     <div class="card card-body">
-                        <p><strong>Type:</strong> '.$explore[1].'</p>
+                        <p><strong>Type:</strong> '.$dataAllWorkout['name'].'</p>
                         <p><strong>Exercices:</strong>
                         <div class="table-responsive">
                         <table class="table table-bordered">
                         <th>Muscle</th><th>Exercice</th><th>Nb séries x Nb reps</th><th>Poids</th>
-                        ';
-        $info = file("model/w".$explore[0]."/info.txt");
-        for ($j = 0; $j < count($info); $j++)
+        ';
+        $infoSQL = recoverTableDataDB("w".$dataAllWorkout['id'],$connect);
+        while ( $dataInfo = pg_fetch_assoc($infoSQL) )
         {
-            $exploreinfo = explode(":",$info[$j]);
-            echo "<tr><td>".$exploreinfo[0]."</td><td>".$exploreinfo[1]."</td>
-            <td>".$exploreinfo[2]."x".$exploreinfo[3]."</td><td>".$exploreinfo[4]."kg</td>";
+            $workoutHTML = $workoutHTML."<tr><td>".$dataInfo['muscle']."</td><td>".$dataInfo['exercice']."</td>
+            <td>".$dataInfo['series']."x".$dataInfo['repetitions']."</td><td>".$dataInfo['poids']."kg</td>";
         }
     
-        echo '</table></div></p></div></div></div>';
-        */
+        $workoutHTML = $workoutHTML.'</table></div></p></div></div></div>';
     }
 
 ?>
--- model/onload.php
+++ model/onload.php
@@ -6,8 +6,6 @@
         if ( !pg_connection_busy($connect) ) pg_send_query($connect,$request);
         $tableSQL = pg_get_result($connect);
         if ( !$tableSQL ) printf("Error model/onload.php > pg_get_result(...)");
-        echo '<br> $connect in function : '.$connect;
-        echo '<br> $tableSQL : '.$tableSQL;
         return $tableSQL;
     }
     
