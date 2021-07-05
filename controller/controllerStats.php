ff7de228061bb6930a05f88fa3246bc3c95f1f98
Youri <yourinovikov@gmail.com>
2021-07-05 14:37:44 +0200

    upd Stats

--- controller/controllerStats.php
+++ controller/controllerStats.php
@@ -1,28 +1,28 @@
 <?php
-    $nbW = $_GET['w'];
+    $idW = $_GET['w'];
     $statsHTML = "";
-    $info = file("model/w".$nbW."/info.txt");
-    for ( $i = 0; $i < count($info); $i++ )
+    $request = "SELECT * FROM public.workout WHERE id=".$idW.";";
+    $workoutSQL = requestDB($request,$connect);
+    while ( $dataWorkout = pg_fetch_assoc($workoutSQL) )
     {
-        $exploreinfo = explode(":",$info[$i]);
-        $muscle = $exploreinfo[0];
-        $exercice = $exploreinfo[1];
-        $nbseries = $exploreinfo[2];
-        $nbrep = $exploreinfo[3];
-        $poids = $exploreinfo[4];
+        $muscle = $dataWorkout['muscle'];
+        $exercice = $dataWorkout['exercice'];
+        $nbseries = $dataWorkout['series'];
+        $nbrep = $dataWorkout['repetitions'];
+        $poids = $dataWorkout['poids'];
 
-        $stats = file("model/w".$nbW."/stats/".strtolower($muscle).".txt");
+        $request = "SELECT * FROM public.stats WHERE id=".$idW." AND muscle='".$muscle."';";
+        $statsSQL = requestDB($request,$connect);
         $statsHTML =  $statsHTML.'<div class="badge bg-dark text-wrap">'.$muscle.' - '.$exercice.' - '.$nbseries.'x'.$nbrep.'</div>
         <div class="table-responsive">    
         <table class="table table-bordered">
             <th>Date</th><th>Nb reps</th><th>Poids</th><th>Niveau</th>';
-        for ( $j = 0; $j < count($stats); $j++ )
+        while ( $dataStats = pg_fetch_assoc($statsSQL) )
         {
-            $explorestats = explode(":",$stats[$j]);
-            $date = $explorestats[0];
-            $statspoids = $explorestats[1];
-            $statsnbrep = $explorestats[2] + $explorestats[3] + $explorestats[4] + $explorestats[5];
-            $statsnbreptext = $explorestats[2].":".$explorestats[3].":".$explorestats[4].":".$explorestats[5];
+            $date = $dataStats['date'];
+            $statspoids = $dataStats['poids'];
+            $statsnbrep = $dataStats['serie1'] + $dataStats['serie2'] + $dataStats['serie3'] + $dataStats['serie4'];
+            $statsnbreptext = $dataStats['serie1'].":".$dataStats['serie2'].":".$dataStats['serie3'].":".$dataStats['serie4'];
             if ( $statsnbrep < 24)
                 $mark = '<i class="bi text-danger bi-exclamation-triangle-fill"></i>';
             else if ( $statsnbrep >= 24 && $statsnbrep < 32)
