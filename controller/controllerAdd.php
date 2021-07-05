e48a61c98ad2e87916917abbe67cdde188aadc93
Youri <yourinovikov@gmail.com>
2021-07-05 11:19:40 +0200

    upd

--- controller/controllerAdd.php
+++ controller/controllerAdd.php
@@ -72,14 +72,13 @@
     function getExercice($nbW,$nbEx,$connect)
     {
         $tab = array();
-        echo '<br>$nbW : '.$nbW;
-        echo '<br>$nbEx : '.$nbEx;
         $request = "SELECT * FROM public.w".$nbW." WHERE id=".$nbEx.";";
+        echo '<br>$request : '.$request;
         if ( !pg_connection_busy($connect) ) pg_send_query($connect,$request);
         $infoSQL = pg_get_result($connect);
         if ( !$infoSQL ) printf('Error $exSQL');
         $infoSQL = pg_fetch_assoc($infoSQL);
-        echo $infoSQL['muscle'];
+        echo '<br>$infoSQL["muscle"] : '.$infoSQL['muscle'];
         $tab[0] = $infoSQL['muscle'];
         $tab[1] = $infoSQL['exerice'];
         $tab[2] = $infoSQL['series'];
