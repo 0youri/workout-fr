052ef23a74d4dd9d50fdeeee66cbe33dde985b87
Youri <yourinovikov@gmail.com>
2021-07-06 14:47:29 +0200

    upd

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ function editWorkout(id, muscle)
     td_poids.innerHTML = 
     `
         <form method="POST" id="formEdit" action="index.php?page=workout&w=${id}&muscle=${muscle}">
-        <input name="editP" id="editP" class="border border-dark" value="${value}">
+        <input name="editP" class="form-control" size="3" id="editP" width="10px" class="border border-dark" value="${value}">
         </form>
     `;
     td_poids_boutons.innerHTML = `
