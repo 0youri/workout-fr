843766f37679b99c2def4a988e426f90e099bddf
Youri <yourinovikov@gmail.com>
2021-07-07 21:18:45 +0200

    Update controllerAdd.php

--- controller/controllerAdd.php
+++ controller/controllerAdd.php
@@ -29,7 +29,7 @@
             $serie2 = $_POST['serie2'];
             $serie3 = $_POST['serie3'];
             $serie4 = $_POST['serie4'];
-            if ( $_POST['poids'] ) $poids = $_POST['poids'];
+            $poids = $_POST['poids'];
             else $poids = $_GET['poids'];
             $values = "'".$today."',".$serie1.",".$serie2.",".$serie3.",".$serie4.",'".$poids."','".$muscle."'";
             addTableDataDB($id,$values,$connect);
@@ -63,7 +63,8 @@
         $lien = 'index.php?page=add&w='.$nbW.'&etat='.($_GET['etat']+1).'&muscle='.$muscle.'&poids='.$poids;
 
         $titleCard = '<form method="POST" id="formAdd" enctype="multipart/form-data" action="'.$lien.'">
-            <h5 class="card-title">['.$muscle.'] '.$exercice.' | '.$nbseries.'x'.$nbrep.' | <span name="poids">'.$poids.'</span>kg</h5>';
+            <input style="display:none;" name="poids" value="'.$poids.'"></div>
+            <h5 class="card-title">['.$muscle.'] '.$exercice.' | '.$nbseries.'x'.$nbrep.' | '.$poids.'kg</h5>';
         $textCard = '<p class="card-text"><div class="row">';
         for ($i = 0; $i < $nbseries; $i++)
         {
@@ -71,6 +72,7 @@
             <input class="border border-dark container" name="serie'.($i+1).'" id="serie'.($i+1).'"></div>';
         }
         $textCard = $textCard.'</div></p>';
+        
         $boutonCard = '<button type="button" class="btn btn-dark" onclick="sumbitFormAdd();">Valider</bouton></form>';
         $viewCard = $titleCard.$textCard.$boutonCard;
  
