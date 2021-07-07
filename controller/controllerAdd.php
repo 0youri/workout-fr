d77494de90660d2d2b34da2887f0bdc726be934e
Youri <yourinovikov@gmail.com>
2021-07-07 21:22:21 +0200

    Update controllerAdd.php

--- controller/controllerAdd.php
+++ controller/controllerAdd.php
@@ -63,7 +63,6 @@
         $lien = 'index.php?page=add&w='.$nbW.'&etat='.($_GET['etat']+1).'&muscle='.$muscle.'&poids='.$poids;
 
         $titleCard = '<form method="POST" id="formAdd" enctype="multipart/form-data" action="'.$lien.'">
-            <input style="display:none;" name="poids" value="'.$poids.'">
             <h5 class="card-title">['.$muscle.'] '.$exercice.' | '.$nbseries.'x'.$nbrep.' | '.$poids.'kg</h5>';
         $textCard = '<p class="card-text"><div class="row">';
         for ($i = 0; $i < $nbseries; $i++)
