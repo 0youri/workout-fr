3c5b07a8713ce4b2f6f4f66760755442d4e27f07
Youri <yourinovikov@gmail.com>
2021-07-07 12:07:46 +0200

    Update viewMenu.php

--- view/viewMenu.php
+++ view/viewMenu.php
@@ -12,12 +12,10 @@
                     aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
+                    <div  class="collapse navbar-collapse" id="navbarSupportedContent">
                     -->
                     <div>
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link" href="index.php">Accueil</a>
-                            </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="index.php?page=workout">Workout</a>
                             </li>
