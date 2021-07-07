468f7b9c2523af36c747f36d27e32fe3d582d889
Youri <yourinovikov@gmail.com>
2021-07-07 12:04:15 +0200

    Update viewMenu.php

--- view/viewMenu.php
+++ view/viewMenu.php
@@ -1,16 +1,18 @@
 <div class="container-fluid bg-dark">
             <header>
                 <h1 class="fw-bold text-white" style="text-align: center">
-                    <a class="link-light" style="text-decoration:none" href="index.php">Workout-FR</a>
+                    <a class="link-light" style="text-decoration:none" href="index.php">workout-fr</a>
                 </h1>
             </header>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-top">
                 <div class="container-fluid">
-                    <button class="" type="button" data-bs-toggle="collapse" 
+                    <!--
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
                     aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
+                    -->
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
