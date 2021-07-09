9b71c2d26a0c5f24a584a969532dbdfdd81d3403
Youri <yourinovikov@gmail.com>
2021-07-09 11:04:07 +0200

    Update viewAccueil.php

--- view/viewAccueil.php
+++ view/viewAccueil.php
@@ -1,10 +1,10 @@
 <br>
-<div class="contrainer">
-    <div class="card" style="text-align: center;">
+<div class="contrainer row">
+    <div class="col card" style="text-align: center;">
     <a class="btn btn-dark" href="index.php?page=workout">Workout</a>
     </div>
     <br>
-    <div class="card" style="text-align: center;">
+    <div class="col card" style="text-align: center;">
     <a class="btn btn-dark" href="index.php?page=food">Food</a>
     </div>
 </div>
