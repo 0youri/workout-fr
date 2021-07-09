aacd19deecea3ae50d0c889c4da8c25ea3aa8b4d
Youri <yourinovikov@gmail.com>
2021-07-09 09:59:51 +0200

    Update controllerWorkout.php

--- controller/controllerWorkout.php
+++ controller/controllerWorkout.php
@@ -18,16 +18,16 @@
         
         $workoutHTML = $workoutHTML.'<div id="w'.$dataAllWorkout['id'].'">
             <br>
-            <div class="card-body border rounded ">
+            <div class="card-body bg-dark text-white border rounded ">
                 <div class="position-relative">
                     <div class="text-black fs-5">
                         <strong>#'.$dataAllWorkout['id'].' - '.$dataAllWorkout['type'].'</strong>
                         <span class="position-absolute end-0">
-                            <button class="btn btn-dark bi bi-info-circle-fill" type="button" id="infow'.$dataAllWorkout['id'].'" 
+                            <button class="btn btn-dark border-light bi bi-info-circle-fill" type="button" id="infow'.$dataAllWorkout['id'].'" 
                             onclick="AfficherCollapse(`collapsew'.$dataAllWorkout['id'].'`);"></button>
-                            <a class="btn btn-dark bi bi-bar-chart-fill" id="statsw'.$dataAllWorkout['id'].'" 
+                            <a class="btn btn-dark border-light bi bi-bar-chart-fill" id="statsw'.$dataAllWorkout['id'].'" 
                             href="index.php?page=stats&w='.$dataAllWorkout['id'].'"></a>
-                            <a class="btn btn-dark bi bi-plus-circle-fill" id="addw'.$dataAllWorkout['id'].'" 
+                            <a class="btn btn-dark border-light bi bi-plus-circle-fill" id="addw'.$dataAllWorkout['id'].'" 
                             href="index.php?page=add&w='.$dataAllWorkout['id'].'&etat=-1"></a>
 
                         </span>
@@ -52,7 +52,7 @@
             <td>".$dataInfo['exercice']."</td>
             <td>".$dataInfo['series']."x".$dataInfo['repetitions']."</td>
             <td id='td-poids".$dataAllWorkout['id'].$dataInfo['rank']."' 
-            name='td-poids".$dataAllWorkout['id'].$dataInfo['rank']."'>".$dataInfo['poids']."</td>
+            name='td-poids".$dataAllWorkout['id']."'>".$dataInfo['poids']."</td>
             <td id='td-poids-boutons".$dataAllWorkout['id'].$dataInfo['rank']."'>
                 <button class='btn btn-dark bi bi-gear-fill' type='button' 
                 onclick='editWorkout(`".$dataAllWorkout['id'].$dataInfo['rank']."`,`".$dataInfo['muscle']."`);'></button>
