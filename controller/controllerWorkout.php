68cb11142530bda80a10fd7b4c013ca2cdfcd38f
Youri <yourinovikov@gmail.com>
2021-08-04 21:27:10 +0200

    UPD

--- controller/controllerWorkout.php
+++ controller/controllerWorkout.php
@@ -25,7 +25,7 @@
             
             $request = "UPDATE public.workout SET 
             exercice='".$exercice."', series=".$series.", repetitions=".$repetitions."
-            WHERE id=".$id." AND rank=".$rank." AND muscle='".$muscle."';";
+            WHERE id=".$id." AND muscle='".$muscle."';";
             echo $request;
             requestDB($request,$connect);
             if ( $_POST['checkbox-edit'] == "on" )
