c412d513215a4e930b86789c138877686ae16bfe
Youri <yourinovikov@gmail.com>
2021-08-05 14:59:59 +0200

    Update controllerAddStats.php

--- controller/controllerAddStats.php
+++ controller/controllerAddStats.php
@@ -51,6 +51,8 @@
     // Finish workout
     else if ( $_GET['state'] == $_GET['stateF'])
     {
+        $noW = $_GET['w'];
+        $typeW = $_GET['typeW'];
         $viewCard = '<h5 class="card-title">Workout is end</h5>
         <a href="index.php?page=workout" class="btn btn-danger">To end</a>';
     }
