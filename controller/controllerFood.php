dcaa4d19c78365d05020c6bc21c89ec417425c30
Youri <yourinovikov@gmail.com>
2021-08-06 18:28:03 +0200

    UPD

--- controller/controllerFood.php
+++ controller/controllerFood.php
@@ -3,39 +3,39 @@
     $request = "SELECT * FROM public.food WHERE type='lunch' ORDER BY rank;";
     $lunchSQL = requestDB($request,$connect);
     
-    $lipidesAll = $glucidesAll = $protAll = $kcalAll = 
-    $lunchKcal = $lunchLipides = $lunchGlucides = $lunchProt = 0;
+    $fatsAll = $carbsAll = $proteinAll = $kcalAll = 
+    $lunchKcal = $lunchfats = $lunchcarbs = $lunchprotein = 0;
     
     while ( $dataLunch = pg_fetch_assoc($lunchSQL) )
     {
-        $aliment = $dataLunch['aliment'];
-        $quantite = $dataLunch['quantite'];
+        $aliment = $dataLunch['food'];
+        $quantite = $dataLunch['amount'];
         $kcal = ( $quantite * $dataLunch['kcal'] ) / 100;
-        $lipides = ( $quantite * $dataLunch['lipides'] ) / 100;
-        $glucides = ( $quantite * $dataLunch['glucides'] ) / 100;
-        $prot = ( $quantite * $dataLunch['proteines'] ) / 100;
+        $fats = ( $quantite * $dataLunch['fats'] ) / 100;
+        $carbs = ( $quantite * $dataLunch['carbs'] ) / 100;
+        $prot = ( $quantite * $dataLunch['protein'] ) / 100;
         $lunchHTML = $lunchHTML.
         "<tr>
             <td>".$aliment."</td>
-            <td>".$kcal."</td><td>".$lipides."</td><td>".$glucides."</td><td>".$prot."</td>
+            <td>".$kcal."</td><td>".$fats."</td><td>".$carbs."</td><td>".$protein."</td>
         </tr>";
         $kcalAll = $kcalAll + $kcal;
-        $lipidesAll = $lipidesAll + $lipides;
-        $glucidesAll = $glucidesAll + $glucides;
-        $protAll = $protAll + $prot;
+        $fatsAll = $fatsAll + $fats;
+        $carbsAll = $carbsAll + $carbs;
+        $proteinAll = $proteinAll + $protein;
     }
     $lunchKcal = $kcalAll;
-    $lunchLipides = $lipidesAll;
-    $lunchGlucides = $glucidesAll;
-    $lunchProt = $protAll;
+    $lunchfats = $fatsAll;
+    $lunchcarbs = $carbsAll;
+    $lunchprotein = $proteinAll;
 
     $lunchHTML = $lunchHTML.
     "<tr class='table-light'>
         <td><strong>Total lunch</strong></td>
         <td><strong>".$lunchKcal."</strong></td>
-        <td><strong>".$lunchLipides."</strong></td>
-        <td><strong>".$lunchGlucides."</strong></td>
-        <td><strong>".$lunchProt."</strong></td>
+        <td><strong>".$lunchfats."</strong></td>
+        <td><strong>".$lunchcarbs."</strong></td>
+        <td><strong>".$lunchprotein."</strong></td>
     </tr>";
 
     $request = "SELECT * FROM public.food WHERE type='dinner' ORDER BY rank;";
@@ -46,28 +46,28 @@
         $aliment = $dataDinner['aliment'];
         $quantite = $dataDinner['quantite'];
         $kcal = ( $quantite * $dataDinner['kcal'] ) / 100;
-        $lipides = ( $quantite * $dataDinner['lipides'] ) / 100;
-        $glucides = ( $quantite * $dataDinner['glucides'] ) / 100;
-        $prot = ( $quantite * $dataDinner['proteines'] ) / 100;
+        $fats = ( $quantite * $dataDinner['fats'] ) / 100;
+        $carbs = ( $quantite * $dataDinner['carbs'] ) / 100;
+        $prot = ( $quantite * $dataDinner['protein'] ) / 100;
         $dinnerHTML = $dinnerHTML.
         "<tr>
             <td>".$aliment."</td>
-            <td>".$kcal."</td><td>".$lipides."</td><td>".$glucides."</td><td>".$prot."</td>
+            <td>".$kcal."</td><td>".$fats."</td><td>".$carbs."</td><td>".$prot."</td>
         </tr>";
         $kcalAll = $kcalAll + $kcal;
-        $lipidesAll = $lipidesAll + $lipides;
-        $glucidesAll = $glucidesAll + $glucides;
+        $fatsAll = $fatsAll + $fats;
+        $carbsAll = $carbsAll + $carbs;
         $protAll = $protAll + $prot;
     }
     $dinnerHTML = $dinnerHTML.
     "<tr class='table-light'>
         <td><strong>Total dinner</strong></td>
         <td><strong>".($kcalAll-$lunchKcal)."</strong></td>
-        <td><strong>".($lipidesAll-$lunchLipides)."</strong></td>
-        <td><strong>".($glucidesAll-$lunchGlucides)."</strong></td>
-        <td><strong>".($protAll-$lunchProt)."</strong></td>
+        <td><strong>".($fatsAll-$lunchfats)."</strong></td>
+        <td><strong>".($carbsAll-$lunchcarbs)."</strong></td>
+        <td><strong>".($proteinAll-$lunchprotein)."</strong></td>
     </tr>";
 
-    $totalHTML = "<tr> <td>~".$kcalAll."</td> <td>~".$lipidesAll."</td>
-    <td>~".$glucidesAll."</td> <td>~".$protAll."</td> </tr>";
+    $totalHTML = "<tr> <td>~".$kcalAll."</td> <td>~".$fatsAll."</td>
+    <td>~".$carbsAll."</td> <td>~".$proteinAll."</td> </tr>";
 ?>
