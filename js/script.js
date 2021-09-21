5274420f22496c5168fcf731a082cd455f8d67b1
Youri <yourinovikov@gmail.com>
2021-09-21 15:06:07 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -45,8 +45,8 @@ function editWorkout(id)
 {
     const type = document.getElementById(`span-workout-type-${id}`).innerHTML;
     alert(id);
-    document.getElementById('input-edit-workout-id') = Number(id);
-    document.getElementById('input-edit-workout-type') = type;
+    document.getElementById('input-edit-workout-id').value = Number(id);
+    document.getElementById('input-edit-workout-type').value = type;
     document.getElementById('formEditWorkout').innerHTML += 
     `<input style="display:none;" name="w" value="${id}">`;
     // Entete du formulaire
