85786e35f70ff711f406ac923420c385c8076dc3
Youri <yourinovikov@gmail.com>
2021-11-14 14:59:18 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -227,17 +227,6 @@ function resetFormEditExercise()
 
 }
 
-document.getElementById('checkbox-formeditexercise-delete-exercise').onchange = () => checkBox();
-
-function checkBox()
-{
-    if ( document.getElementById('checkbox-formeditexercise-delete-exercise').checked === true  )
-    {
-        alert(`Attention, you chose "Delete exercise"!`);
-    }
-}
-
-
 
 // ---------------------------------------------
 
