0a5291366ff1373b870f848029a809bedd932ae1
Youri <yourinovikov@gmail.com>
2021-11-17 10:20:27 +0100

    Update controllerAddStats.php

--- controller/controllerAddStats.php
+++ controller/controllerAddStats.php
@@ -19,7 +19,7 @@
             for ($i = 1; $_POST['serie'.$i]; $i++)
             {
                 echo "here1.1<br>";
-                $series += $_POST['serie'.$i];
+                $series = $_POST['serie'.$i];
                 echo "here1.2<br>";
                 $series += ",";
                 echo "here1.3<br>";
