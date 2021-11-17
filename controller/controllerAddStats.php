87a3fe3477efc810b70a433df79dc0241b6f3be9
Youri <yourinovikov@gmail.com>
2021-11-17 11:11:49 +0100

    Update controllerAddStats.php

--- controller/controllerAddStats.php
+++ controller/controllerAddStats.php
@@ -12,16 +12,15 @@
             $id = $_GET['w'];
             $muscle = $_POST['muscle'];
             $rank = $_POST['rank'];
-            $today = date("Y-m-d");
+            $date = date("Y-m-d");
             $series = '';
             for ($i = 1; $_POST["serie$i"]; $i++)
             {
                 $series .= $_POST["serie$i"].",";
             }
-            echo "dsq";
             $weight = $_POST['weight'];
             $request = "
-            INSERT INTO public.stats VALUES ($id,$series'$weight','$muscle',$rank);
+            INSERT INTO public.stats VALUES ($id,$date,$series'$weight','$muscle',$rank);
             ";
             echo $request;
             requestDB($request,$connect);
