031338a67c3f2f68374a2db1c8e080403a13c41c
Youri <yourinovikov@gmail.com>
2021-11-17 10:49:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" 
         integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.js"></script>
-        <script type="text/javascript" src="js/script.js?0"></script>
+        <script type="text/javascript" src="js/script.js?1"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     </head>
         
