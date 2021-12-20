3c76548f741405d9ebd1ecf531ad5c7abe67e32c
Youri <yourinovikov@gmail.com>
2021-12-20 15:05:47 +0100

    Update controllerAddStats.php

--- controller/controllerAddStats.php
+++ controller/controllerAddStats.php
@@ -53,6 +53,7 @@
 
         $link = 'index.php?page=addstats&id='.$noW.'&rankW='.$rankW.'&typeW='.$typeW.'&stateF='.$stateF.'&state=0';
         header("Location : $link");
+        exit();
     }
     // Finish workout
     else if ( $_GET['state'] == $_GET['stateF'])
